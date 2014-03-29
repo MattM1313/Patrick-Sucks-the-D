9d753b960d12f24697713ad08e88ead93098c911
Vinny Crupi <vinnycrupi@hotmail.com>
2014-03-29 19:04:57 -0400

    Created player prefab
    - Wheel with a hamster inside.
    - Hamster tumbles freely in wheel
    - Control (left/right) only active when hamster touches wheel
    - Up arrow flips hamster if stuck
    - Camera follows wheel

---
+++ Physics Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float torqueForce;
+    public GameObject wheel;
+    public bool touchingWheel;
+
+    private const int FLIP_DELAY = 2;
+    private bool canFlip = true;
+
+	void Start () {
+	
+	}
+
+    void Update() {
+        if (touchingWheel) {
+            if (Input.GetAxis("Horizontal") != 0) {
+                wheel.rigidbody2D.AddTorque(torqueForce * -Input.GetAxis("Horizontal"));
+            }
+        }
+        else {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                StartCoroutine(FlipOver());
+            }
+        }
+
+    }
+
+    IEnumerator FlipOver() {
+        canFlip = false;
+
+        Vector2 flipForce = new Vector2(1, -1) * rigidbody2D.mass * 100;
+        rigidbody2D.AddForceAtPosition(transform.TransformDirection(flipForce), transform.TransformPoint(Vector2.right));
+
+        yield return new WaitForSeconds(FLIP_DELAY);
+        canFlip = true;
+    }
+
+}
---
+++ Physics Final/Assets/Scripts/PlayerFeet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFeet : MonoBehaviour {
+
+    public CircleCollider2D innerWheel;
+    public PlayerController player;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col == innerWheel) {
+            player.touchingWheel = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col == innerWheel) {
+            player.touchingWheel = true;
+        }
+    }
+
+}
