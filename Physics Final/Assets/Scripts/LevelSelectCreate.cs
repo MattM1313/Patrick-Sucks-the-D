5a1a0ad68c2f8ae659a54676455dc67a069c31a3
MattM1313 <matt.mayo1313@gmail.com>
2014-04-16 16:35:37 -0400

    LevelSelect Added

---
+++ Physics Final/Assets/Scripts/LevelSelectCreate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectCreate : MonoBehaviour {
+
+	public int selGridInt;
+	private string[] selStrings = new string[] {"Grid 1", "Grid 2", "Grid 3", "Grid 4", "Grid 5", "Grid 6", "Grid 7", "Grid 8", "Grid 9", "Grid 10", "Grid 11", "Grid 12", "Grid 13", "Grid 14", "Grid 15"};
+
+	void OnGUI() 
+	{
+		selGridInt = GUI.SelectionGrid(new Rect(25, 25, 575, 300), selGridInt, selStrings, 5);
+		Application.LoadLevel(selGridInt);
+	}
+
+	void OnMouseOver()
+	{
+
+	}
+
+}
