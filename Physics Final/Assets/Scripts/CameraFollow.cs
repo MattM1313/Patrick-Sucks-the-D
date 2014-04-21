34494b8a96b7ca7b2ec9a88422be58c0ce7e33e0
Vinny Crupi <vinnycrupi@hotmail.com>
2014-04-20 22:07:31 -0400

    Camera distance

---
+++ Physics Final/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public bool followTarget;
+    public float cameraDist = 10;
+
+    void Start() {
+        if (followTarget) {
+            transform.localPosition = new Vector3(target.transform.localPosition.x, target.transform.localPosition.y, -cameraDist);
+        }
+    }
+
+	void Update () {
+        if (followTarget) {
+            //transform.localPosition = Vector3.Slerp(transform.localPosition, new Vector3(target.transform.localPosition.x, target.transform.localPosition.y, -cameraDist), Time.deltaTime);
+
+            transform.localPosition = new Vector3(target.transform.localPosition.x, target.transform.localPosition.y, -cameraDist);
+        }
+	}
+}
